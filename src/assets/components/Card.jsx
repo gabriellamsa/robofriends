cedc5377c4a86637aaf344e7d2ac5bf876166d4d
gabriellamsa <gabii.marcelly@gmail.com>
2025-03-19 18:22:06 -0500

    Add CardList component and update Card with dynamic robot images

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -1,12 +1,12 @@
-export const Card = ({ name, email }) => {
+export const Card = ({ id, name, email }) => {
   return (
-    <div className="bg-green-200 w-64 rounded-lg p-4 m-4 shadow-lg transition-transform transform hover:scale-105">
+    <div className="bg-teal-200 w-64 rounded-lg p-4 m-4 shadow-lg transition-transform transform hover:scale-105">
       <img
         alt="robots"
-        src={`https://robohash.org/test?size=200x200`}
-        className="w-full h-40 object-cover rounded-md"
+        src={`https://robohash.org/${id}?size=200x200`}
+        className="w-full h-60 object-cover rounded-md"
       />
-      <div className="text-center mt-4">
+      <div className="text-center mt-6">
         <h2 className="text-lg font-semibold">{name}</h2>
         <p className="text-sm text-gray-600">{email}</p>
       </div>
---
+++ src/assets/components/CardList.jsx
@@ -0,0 +1,17 @@
+import { Card } from "./Card";
+
+export const CardList = ({ robots }) => {
+  const cardComponent = robots.map((user, i) => {
+    return (
+      <Card
+        key={i}
+        id={robots[i].id}
+        name={robots[i].name}
+        email={robots[i].email}
+      />
+    );
+  });
+  return (
+    <div className="flex flex-wrap justify-center p-4">{cardComponent} </div>
+  );
+};
